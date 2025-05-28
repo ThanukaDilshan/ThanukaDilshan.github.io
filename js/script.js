b050873761aa319750d86ca7f1f43858198d9da0
Thanuka Dilshan <150824334+ThanukaDilshan@users.noreply.github.com>
2025-05-28 17:31:40 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -170,7 +170,7 @@ const buttonDown = document.getElementById("downloadBtn");
 
 buttonDown.addEventListener("click", function() {
     const link = document.createElement("a");
-    link.href = "files/thanuka_dilshan_cv.pdf";
+    link.href = "./files/thanuka_dilshan_cv.pdf";
     link.download = "thanuka_dilshan_cv.pdf";
     link.click();
 });
